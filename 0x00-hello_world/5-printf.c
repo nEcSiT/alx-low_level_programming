6ce4ce6056b588b05211eb42b60b955b8115dbe7
nEcSiT <nicholasdornyo11@gmail.com>
2023-02-17 16:41:39 +0000

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a code that print the value of a string
+ * Prints out, with a proper grammar
+ * Return: 0
+*/
+int main(void)
+{
+printf("%s\n", "with proper grammar,but the outcome is a piece of art");
+return (0);
+}
