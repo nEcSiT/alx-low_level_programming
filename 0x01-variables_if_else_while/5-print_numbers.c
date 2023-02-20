5a20d5e6383e2ae93a1417694f020dfeb9a0f1ad
nEcSiT <nicholasdornyo11@gmail.com>
2023-02-20 18:20:34 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (successful)
+ */
+
+int main(void)
+
+{
+	char a = '0';
+		if (a <= 10)
+		{
+			putchar(a);
+				a++;
+		}
+		{
+			putchar('\n');
+		}
+		return (0);
+}
