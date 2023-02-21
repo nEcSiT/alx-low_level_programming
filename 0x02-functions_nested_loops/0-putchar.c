7be5f3ab93493367b943f005308e118b25550387
nEcSiT <nicholasdornyo11@gmail.com>
2023-02-21 23:14:34 +0000

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - A program the prints _putchar
+ *
+ * Return: 0 (successful);
+ */
+
+int main(void)
+
+{
+int letter;
+	getchar();
+	letter = '_';
+	putchar(letter);
+
+	letter = 'p';
+	putchar(letter);
+
+	letter = 'u';
+	putchar(letter);
+
+	letter ='t';
+	putchar(letter);
+
+	letter = 'c';
+	putchar(letter);
+
+	letter = 'h';
+	putchar(letter);
+
+	letter = 'a';
+	putchar(letter);
+
+	letter = 'r';
+	putchar(letter);
+
+		putchar('\n');
+	return (0);}
