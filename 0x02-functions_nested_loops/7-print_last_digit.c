03d9163062959f48e3a70ad1c52a337dc7e12fe4
nEcSiT <nicholasdornyo11@gmail.com>
2023-02-22 16:59:38 +0000

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - using the modulos operartor to print
+ * @n: the char in display
+ *Return: the last digit
+ */
+
+int print_last_digit(int n)
+
+{
+	_putchar(n % 10);
+		return (n % 10);
+}
