994c0364183e0c60ed6ecfaa9e222e59139c1b01
nEcSiT <nicholasdornyo11@gmail.com>
2023-02-22 23:31:41 +0000

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,28 +7,37 @@
  */
 
 int print_sign(int n)
-{
-	int r;
 
-	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+{
+	if (n > 0)
+
+	{
+
+		_putchar('+');
+
+		return (1);
+
+	}
+
+	else if (n < 0)
+
+	{
+
+		_putchar ('-');
+
+		return (-1);
+
+	}
+
+	else
+
+	{
+
+		_putchar('0');
+
+		return (0);
+
+	}
+
+
 }
