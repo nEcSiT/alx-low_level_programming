604f82305ee8e5c4730a4ebad512b7e51ecdf886
nEcSiT <nicholasdornyo11@gmail.com>
2023-02-22 02:10:43 +0000

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - Prints 10x the alphabets
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int nic = 0;
+	int dor;
+
+		while (nic <= 9)
+		{
+			dor = 'a';
+			while (dor <= 'z')
+				_putchar(dor);
+					dor++;
+			{
+
+			}
+			_putchar('\n');
+				nic++;
+		}
+
+}
