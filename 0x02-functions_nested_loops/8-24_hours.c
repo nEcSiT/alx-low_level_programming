144e796a699368e55b46730f6aa28b9b9547e2ed
nEcSiT <nicholasdornyo11@gmail.com>
2023-02-23 20:34:11 +0000

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,36 +12,35 @@ int i, j, k, m;
 
 for (i = 0; i <= 2; i++)
 
-	{
+{
 
 	for (j = 0; j <= 9; j++)
 
-		{
+	{
 
 		for (k = 0; k <= 5; k++)
 
-			{
+		{
 
 			for (m = 0; m <= 9; m++)
 
-	{
+{
 
 	if (i == 2 && j > 3)
 	{
 	break;
 	}
 
-				_putchar(i + '0');
-			_putchar(j + '0');
-		_putchar(':');
+	_putchar(i + '0');
+	_putchar(j + '0');
+	_putchar(':');
 	_putchar(k + '0');
-_putchar(m + '0');
-_putchar('\n');
-	}
-
-				}
+	_putchar(m + '0');
+	_putchar('\n');
+}
 
 		}
 
 	}
+
 }
