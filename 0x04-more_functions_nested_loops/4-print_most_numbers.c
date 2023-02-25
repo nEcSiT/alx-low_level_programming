c59fe9757e55093aa33cd2b4e6522152b72bde98
nEcSiT <nicholasdornyo11@gmail.com>
2023-02-25 03:21:16 +0000

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * print_most_numbers - a function that prints the numbers
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+
+{
+	int a = 0;
+
+	while (a <= 9 && (a != 2 && a != 4))
+	{
+		_putchar(a + '0');
+			a++;
+	}
+		_putchar('\n');
+}
