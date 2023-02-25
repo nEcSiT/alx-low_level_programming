cfe03cc34a1487315875f178e0dd33332f777bc8
nEcSiT <nicholasdornyo11@gmail.com>
2023-02-25 02:59:18 +0000

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,9 +10,10 @@ void print_numbers(void)
 
 {
 int i;
-		while (i = 0, i < 10)
-		_putchar(i + 0);
-			i++;
-		_putchar('\n');
-
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
 }
