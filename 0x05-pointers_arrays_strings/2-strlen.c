011ca9ee661401d588e9490d862cc12dfb616f7d
nEcSiT <nicholasdornyo11@gmail.com>
2023-02-27 15:03:09 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - return a value
+ * @c: the int name
+ * Return: 0
+ */
+
+int _strlen(char *s)
+
+{
+	int cat = 0;
+	for (; *s != '\0'; s++)
+	cat++;
+
+return (cat);
+}
