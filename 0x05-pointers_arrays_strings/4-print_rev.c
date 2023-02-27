daf37800b966a6e9d838492317b151ae6d91f831
nEcSiT <nicholasdornyo11@gmail.com>
2023-02-27 15:47:10 +0000

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - print in reverse
+ * @s: the int type
+ * Return: 0
+ */
+
+void print_rev(char *s)
+
+{
+	for (; *s != '\0'; s--)
+
+		putchar(*s);
+
+	_putchar('\n');
+}
