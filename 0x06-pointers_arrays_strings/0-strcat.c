b720cca1a142468389556b09df23cd8acfbcbf79
nEcSiT <nicholasdornyo11@gmail.com>
2023-03-01 22:36:42 +0000

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatinate text
+ * @dest: the destination file
+ * @src: the source file
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+*dest = '\0';
+
+return (dest);
+}
