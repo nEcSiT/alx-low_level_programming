9cfc2656ad9c9ede1a02aa6f1a6d17eb8972c825
nEcSiT <nicholasdornyo11@gmail.com>
2023-03-02 11:09:47 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,20 @@
 char *_strcat(char *dest, char *src)
 
 {
+	int i = 0, j = 0;
 
-	while (*dest != '\0')
+	while (dest[i] != '\0')
 	{
 		dest++;
+		i++;
 	}
-	while (*src != '\0')
+	while (src[j] != '\0')
 	{
 		*dest = *src;
+
 		dest++;
 		src++;
+		j++;
 	}
 *dest = '\0';
 
