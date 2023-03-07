3e406a69cf777fb60957f296f956104103c1e3e5
nEcSiT <nicholasdornyo11@gmail.com>
2023-03-07 14:30:31 +0000

    pointers aand arrays

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _strpbrk - Searches a string for any of a set of bytes.
+ * _strspn - Gets the length of a prefix substring.
  * @s: The string to be searched.
- * @accept: The set of bytes to be searched for.
+ * @accept: The prefix to be measured.
  *
- * Return: If a set is matched - a pointer to the matched byte.
- * If no set is matched - NULL.
+ * Return: The number of bytes in s which  only of bytes from accept.
  */
-char *_strpbrk(char *s, char *accept)
+
+unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int bytes = 0;
 	int i;
 
 	while (*s)
@@ -17,10 +18,17 @@ char *_strpbrk(char *s, char *accept)
 		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
-				return (s);
+			{
+				bytes++;
+				break;
+			}
+
+			else if (accept[i + 1] == '\0')
+				return (bytes);
 		}
 
 		s++;
 	}
-	return ('\0');
+
+	return (bytes);
 }
