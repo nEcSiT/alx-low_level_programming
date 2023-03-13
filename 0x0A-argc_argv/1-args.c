83202a02c2fb88d34002449632384e847f0e70df
nEcSiT <nicholasdornyo11@gmail.com>
2023-03-13 12:42:08 +0000

    argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the entry point
+ * @argc: number count
+ * @argv: vetor or string
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+
+{
+
+	printf("%d\n", argc);
+
+return (0);
+}
