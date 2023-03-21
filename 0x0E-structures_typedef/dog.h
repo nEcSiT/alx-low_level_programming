aa8c5e3e3b60c9302adc60aefa333d46e39cd132
nEcSiT <nicholasdornyo11@gmail.com>
2023-03-21 12:43:02 +0000

    struct and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,9 +10,9 @@
  * Return:0
  */
 
-struct dog
-{
+	struct dog
+	{
 	char *name;
 	float age;
 	char *owner;
-};
+	};
