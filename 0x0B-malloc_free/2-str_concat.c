c29f41220e48e65f043d7bd1d90f91327e9c505d
nEcSiT <nicholasdornyo11@gmail.com>
2023-04-20 04:10:59 +0000

    Revert "Delete 0x0B-malloc_free directory"
    This reverts commit 428dc3bcc93befc163583ea437fd1e717c05e8b9.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * str_concat - concatinate two strings.
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: null when failed
+ */
+
+char *str_concat(char *s1, char *s2)
+
+{
+	char *concat_str;
+	int i, concat_i = 0, len = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] || s2[i]; i++)
+		len++;
+
+	concat_str = malloc(sizeof(char) * len);
+
+	if (concat_str == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		concat_str[concat_i++] = s1[i];
+
+	for (i = 0; s2[i]; i++)
+		concat_str[concat_i++] = s2[i];
+
+	return (concat_str);
+}
