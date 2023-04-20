23f30515a95e8cb986aad566908ada07c85e9b1b
nEcSiT <nicholasdornyo11@gmail.com>
2023-04-20 04:08:01 +0000

    Revert "Delete 0x13-more_singly_linked_lists directory"
    This reverts commit 6637981e628a3a14d16bd83446d6af83935ed91c.

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - gives the nuber of length
+ * @h: a pointer to the header
+ *
+ * Return: length
+ */
+
+size_t listint_len(const listint_t *h)
+
+{
+	size_t node = 0;
+	{
+		while (h != NULL)
+
+		node++;
+		h = h->next;
+	}
+
+	return (node);
+}
