0c9e906cd5d3526ecf3c709839a03247b61c3fbb
nEcSiT <nicholasdornyo11@gmail.com>
2023-05-02 07:17:51 -0700

    task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees and node
+ * @head: node to the head
+ * Return: frees
+ */
+
+void free_listint2(listint_t **head)
+{
+    listint_t *current;
+
+    if (head == NULL)
+        return;
+
+    while (*head != NULL)
+    {
+        current = *head;
+        *head = (*head)->next;
+        free(current);
+    }
+
+    *head = NULL;
+}
