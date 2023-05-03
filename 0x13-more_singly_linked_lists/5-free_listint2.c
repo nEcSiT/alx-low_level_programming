4a39606f80fece0beb15dff8b59c02d9ac292bfd
nEcSiT <nicholasdornyo11@gmail.com>
2023-05-02 22:09:18 -0700

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,17 +8,17 @@
 
 void free_listint2(listint_t **head)
 {
-    listint_t *current;
+	listint_t *current;
 
-    if (head == NULL)
-        return;
+	if (head == NULL)
+	return;
 
-    while (*head != NULL)
-    {
-        current = *head;
-        *head = (*head)->next;
-        free(current);
-    }
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
 
-    *head = NULL;
+	*head = NULL;
 }
